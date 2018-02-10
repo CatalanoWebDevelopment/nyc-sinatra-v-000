3d4e38bde8868084a45491ec3603052adaf816ae
Parker Catalano <catalano.work@gmail.com>
2018-02-10 18:15:47 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,7 +5,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    @landmark = Landmark.create(params[:landmark])
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
 
     redirect "/landmarks/#{@landmark.id}"
   end
